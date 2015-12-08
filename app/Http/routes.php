89b2dd85be638f58be3d37f10aaae158a470deb0
Lyntor Paul Figueroa <lyntor.figueroa@gmail.com>
2015-12-08 15:02:41 +0800

    Update http routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+/**
+ * WallpaperHub
+ *
+ * This is a website for wallpaper enthusiasts to share amazingly awesome wallpapers on the net.
+ * You can browse and download thousands of free desktop or mobile wallpapers from any device at any time.
+ *
+ * @copyright  Copyright (c) 2015 Lyntor Paul Figueroa
+ * @license    MIT License (See LICENSE file)
+ * @link       http://www.wallpaperhub.cf
+ */
 
-Route::get('/', function () {
+// Send users to the homepage
+Route::get('/', function() {
     return view('welcome');
 });
